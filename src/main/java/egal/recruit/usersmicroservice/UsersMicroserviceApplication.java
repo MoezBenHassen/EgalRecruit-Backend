7c9654adcb94b615315c3ae32ab01e45889f2fba
Med Yacine Khouini <khouini.medyacine@gmail.com>
2023-11-22 23:52:08 +0100

    working auth with KeyCloack

--- src/main/java/egal/recruit/usersmicroservice/UsersMicroserviceApplication.java
+++ src/main/java/egal/recruit/usersmicroservice/UsersMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package egal.recruit.usersmicroservice;
+package egal.recruit.UsersMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
