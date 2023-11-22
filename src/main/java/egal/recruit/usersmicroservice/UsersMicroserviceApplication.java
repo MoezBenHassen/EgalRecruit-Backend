5bb4b6644aecdc4330d5eac851212757c4b513a6
Med Yacine Khouini <khouini.medyacine@gmail.com>
2023-11-22 19:56:06 +0100

    project init

---
+++ src/main/java/egal/recruit/usersmicroservice/UsersMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package egal.recruit.usersmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMicroserviceApplication.class, args);
+	}
+
+}
