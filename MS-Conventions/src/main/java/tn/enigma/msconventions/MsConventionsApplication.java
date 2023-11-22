d120c2f1a597c13e8d262f60d0882270fc2d7d8b
Nassim BenSalah <nassim.bensalah@esprit.tn>
2023-11-22 17:36:25 +0100

    initial commit

---
+++ MS-Conventions/src/main/java/tn/enigma/msconventions/MsConventionsApplication.java
@@ -0,0 +1,13 @@
+package tn.enigma.msconventions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConventionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsConventionsApplication.class, args);
+    }
+
+}
---
+++ MS-Conventions/src/test/java/tn/enigma/msconventions/MsConventionsApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enigma.msconventions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConventionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
